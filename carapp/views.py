612a60dc7b4deff2e79e39e7ef3144f281ddd5fa
raphplaza <plazarafal@gmail.com>
2021-12-03 11:20:09 +0100

    Request methods POST and DELETE added

--- carapp/views.py
+++ carapp/views.py
@@ -1,86 +1,92 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from carapp.serializers import UserSerializer, GroupSerializer, CarSerializer
 from carapp.models import Car, Rate
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class CarViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows cars to be viewed or edited.
-    """
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-
-def car_list(request):
+@api_view(['GET','POST'])
+def car_list(request, format=None):
     """
     List all code cars, or create a new car.
     """
     if request.method == 'GET':
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(data=data)
+        serializer = CarSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def car_detail(request, pk):
+
+@api_view(['GET','POST','DELETE'])
+def car_detail(request, pk,format=None):
     """
     GET, or DELETE a car.
     """
     try:
         car = Car.objects.get(pk=pk)
     except Car.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=404)
 
     if request.method == 'GET':
         serializer = CarSerializer(car)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'DELETE':
         car.delete()
-        return HttpResponse(status=204)
+        return Response(status=204)
 
-"""
-def car_post(request)
-        elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(car, data=data)
+    elif request.method == 'POST':
+        serializer = CarSerializer(car, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        else:
-            return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
+
+"""
+#old views
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    
+    #API endpoint that allows groups to be viewed or edited.
+   
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CarViewSet(viewsets.ModelViewSet):
+   
+    #API endpoint that allows cars to be viewed or edited.
+    
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [permissions.IsAuthenticated]
 """
